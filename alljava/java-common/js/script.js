c54a7bbf2c40b9407d6e326711ab2934abc45bf5
Christian <christian.rossi1198@gmail.com>
2022-11-28 10:43:40 +0100

    More JavaScript

---
+++ alljava/java-common/js/script.js
@@ -0,0 +1,62 @@
+// var string = "Hello";
+// string += " World";
+// console.log(string + "!");
+
+// console.log((5 + 4) / 3);
+// console.log(undefined / 5);
+// function test1 (a) {
+//     console.log( a / 5);
+// }
+// test1()
+
+// var x = 4, y= 4;
+// if (x == y) {
+//     console.log("x is equal to y");
+// }
+// x = "4";
+// if (x == y) {
+//     console.log("x is equal to y");
+// }
+
+// if (x === y) {
+//     console.log("Strict: x is equal to y");
+// }
+// else {
+//     console.log("Strict: x is NOT equal to y");
+// }
+
+// if ( false || null || undefined || "" || 0 || NaN) {
+//     console.log("This line won't ever execute");
+// }
+// else {
+//     console.log("All false");
+// }
+
+// if (true && "hello" && 1 && -1 && "false") {
+//     console.log("All true");
+// }
+
+// function a()
+// {
+//     return
+//     {
+//         name: "Christian"
+//     };
+// }
+
+// function b() {
+//     return {
+//         name: "Christian"
+//     };
+// }
+
+// console.log(a());
+// console.log(b());
+
+var sum = 0;
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+    sum = sum + i;
+}
+console.log("sum of 0 through 10 is: " + sum);
+
